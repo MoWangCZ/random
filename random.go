fc8dcf2c20782b47f71570ffeb309935f9c90f28
H <mrhehuan@foxmail.com>
2019-07-29 14:54:14 +0800

    add random v1

---
+++ random.go
@@ -0,0 +1,14 @@
+package random
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func RandomString() string {
+	rand.Seed(time.Now().UnixNano())
+	rd := rand.Int63()
+	rs := fmt.Sprintf("Rand v1:%d", rd)
+	return rs
+}
