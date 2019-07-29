f024bf99c461b7c2d409ee68147d17bc725d1c8b
H <mrhehuan@foxmail.com>
2019-07-29 16:50:26 +0800

    fix-print + new

--- random.go
+++ random.go
@@ -9,6 +9,6 @@ import (
 func RandomString() string {
 	rand.Seed(time.Now().UnixNano())
 	rd := rand.Int63()
-	rs := fmt.Sprintf("Rand Fix-Print:%d", rd)
+	rs := fmt.Sprintf("Rand Fix-Print New:%d", rd)
 	return rs
 }
